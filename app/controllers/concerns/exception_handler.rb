426b955b3e19e704587ffda1ceb82d966b327093
Petru Cioata <petru.cioata@assist.ro>
2018-08-25 18:16:28 +0300

    Implemented JWT

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,29 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
+
+    rescue_from ActiveRecord::RecordInvalid, with: :_422
+    rescue_from ExceptionHandler::AuthenticationError, with: :_401
+    rescue_from ExceptionHandler::MissingToken, with: :_422
+    rescue_from ExceptionHandler::InvalidToken, with: :_422
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+    private
+
+    def _401(error)
+      json_response({ message: error.message }, :unauthorized)
+    end
+
+    def _422(error)
+      json_response({ message: error.message }, :unprocessable_entity)
     end
   end
 end
